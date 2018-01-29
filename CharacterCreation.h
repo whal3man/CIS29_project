259812cf272aef7c958e199da4805ed124c55621
Friend Trinity Dickinson <ftrinitydickinson@gmail.com>
2018-01-28 22:15:54 -0800

    Functionality put in Player

--- CharacterCreation.h
+++
@@ -1,8 +0,0 @@
-#ifndef CHARACTERCREATION_H_INCLUDED
-#define CHARACTERCREATION_H_INCLUDED
-
-#include "Character.h"
-
-void runCharacterCreation(Character pchar);
-
-#endif // CHARACTERCREATION_H_INCLUDED
