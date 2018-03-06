7dbf19e1259549b19750de96d780ba4ba94104e3
Michael <33842388+SigmaSirius@users.noreply.github.com>
2018-03-05 17:20:19 -0800

    Added a function to strip non-alphabetic characters from a string and then convert it into lowercase. Also added a function to display all valid player commands that can be displayed on demand or whenever the player entered an invalid command.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,46 @@
 #include "include/Map.h"
 using namespace std;
 
+void displayCommands()
+{
+    system("cls");
+    cout << "[w] north: move north\n";
+    cout << "[s] south: move south\n";
+    cout << "[a] west: move west\n";
+    cout << "[d] east: move east\n";
+    cout << "[q] help: display the list of valid commands\n";
+    cout << "[x] quit: quit the game\n";
+    system("pause");
+}
+
+// A function to strip non-alphabetic characters from the start and end of the string.
+// It will also convert all the remaining characters into lowercase.
+string clean(const string& s)
+{
+    string r = "";
+    for (int start = 0; start < s.length(); start++)
+    {
+        if (isalpha(s[start]))
+        {
+            for (int end = s.length()-1; end >= start; end--)
+            {
+                if (isalpha(s[end]))
+                {
+                    r = s.substr(start, end - start + 1);
+                    break;
+                }
+            }
+            break;
+        }
+    }
+
+    for(int i = 0; i < r.size(); i++) {
+        r[i] = tolower(r[i]);
+    }
+
+    return r;
+}
+
 int main()
 {
     const bool DEBUG = true;
@@ -17,7 +57,7 @@ int main()
     Map gameMap(monsterSpawnRate, rows, cols, floors, startingX, startingY, startingZ);
 
     string input = "";
-    while(input != "quit") {
+    while(input != "quit" && input != "x") {
         gameMap.print();
         Tile currentTile = gameMap.playerTile();
         cout << "Walls: ";
@@ -28,24 +68,31 @@ int main()
 
         cout << "\nPlease enter a command.\n";
         cin >> input;
-        for(int i = 0; i < input.size(); i++) {
-            input[i] = tolower(input[i]);
-        }
-        if((input == "up" || input == "north") && !currentTile.checkWall("up"))
+        input = clean(input);
+        if((input == "up" || input == "north" || input == "w") && !currentTile.checkWall("up"))
         {
             pchar.moveUp();
         }
-        else if((input == "down" || input == "south") && !currentTile.checkWall("down"))
+        else if((input == "down" || input == "south" || input == "s") && !currentTile.checkWall("down"))
         {
             pchar.moveDown();
         }
-        else if((input == "left" || input == "west") && !currentTile.checkWall("left"))
+        else if((input == "left" || input == "west" || input == "a") && !currentTile.checkWall("left"))
         {
             pchar.moveLeft();
         }
-        else if((input == "right" || input == "east") && !currentTile.checkWall("right"))
+        else if((input == "right" || input == "east" || input == "d") && !currentTile.checkWall("right"))
         {
             pchar.moveRight();
+        }
+        else if(input == "help" || input == "q")
+        {
+            displayCommands();
+        }
+        else if(input != "x")
+        {
+            cout << "Invalid command.\n";
+            displayCommands();
         }
 
         gameMap.updatePlayerLoc(pchar.getX(), pchar.getY(), pchar.getZ());
