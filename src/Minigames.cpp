c0e25412ecce281adbace1df57a4da1019b4f8e9
Friend Trinity Dickinson <ftrinitydickinson@gmail.com>
2018-03-20 10:08:51 -0700

    Allow exit from password guessing, lambda expression

--- src/Minigames.cpp
+++ src/Minigames.cpp
@@ -16,14 +16,19 @@ bool guessingGame(int low, int high)
         cout << ". ";
         this_thread::sleep_for(chrono::milliseconds(500));
     }
-    cout << "\nGuess the single number password, between " << low << " and " << high << ".\n";
+    cout << "\nGuess the single positive number password, between " << low << " and " << high << ". (-# to exit)\n";
     int numToGuess = randInt(low, high);
     int numGuesses = 10;
     char guess;
     while(numGuesses >= 0)
     {
-        cout << "\nYou have " << numGuesses << " guesses remaining. ";
+        auto printNumGuesses = [=]()
+        {
+            cout << "\nYou have " << numGuesses << " guesses remaining. ";
+        };
+        printNumGuesses();
         cin >> guess;
+        if(guess - 48 < 0) break;
         if(isdigit(guess))
         {
             if(guess - 48 == numToGuess)
@@ -40,7 +45,7 @@ bool guessingGame(int low, int high)
         }
         else cout << "Error: invalid input. Please input a single number.\n";
     }
-    cout << "You ran out of guesses. Better luck next time!\n";
+    if(numGuesses = 0) cout << "You ran out of guesses. Better luck next time!\n";
     system("pause");
     return false;
 }
