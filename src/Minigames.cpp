ada7562de8cd7f5a0facad36cf2252c52f2f11d9
Friend Trinity Dickinson <ftrinitydickinson@gmail.com>
2018-03-20 09:40:46 -0700

    Fix bug in password minigame

--- src/Minigames.cpp
+++ src/Minigames.cpp
@@ -19,14 +19,14 @@ bool guessingGame(int low, int high)
     cout << "\nGuess the single number password, between " << low << " and " << high << ".\n";
     int numToGuess = randInt(low, high);
     int numGuesses = 10;
-    int guess;
+    char guess;
     while(numGuesses >= 0)
     {
         cout << "\nYou have " << numGuesses << " guesses remaining. ";
         cin >> guess;
         if(isdigit(guess))
         {
-            if(guess == numToGuess)
+            if(guess - 48 == numToGuess)
             {
                 cout << "You got it, with " << numGuesses << " guesses remaining!\n";
                 system("pause");
@@ -38,7 +38,7 @@ bool guessingGame(int low, int high)
                 numGuesses--;
             }
         }
-        else cout << "Error: invalid input. Please input a single number.\n"
+        else cout << "Error: invalid input. Please input a single number.\n";
     }
     cout << "You ran out of guesses. Better luck next time!\n";
     system("pause");
